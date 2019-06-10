5194fa8fbf7c0fc25a2288761ef905cae9189020
satinfive <satinfivee@gmail.com>
2019-06-10 16:05:06 +0200

    added python env management

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-f
+import os
+from dotenv import load_dotenv
 from email_server.server import EmailServer
+
+# loads env variables
+load_dotenv()
+
+from_email = os.getenv('EMAIL')
+password = os.getenv('EMAIL_PASSWORD')
+to_email = os.getenv('TO_EMAIL')
 
 server = EmailServer()
 server.login('satinfivee@gmail.com', 'AJOL_1809')
@@ -7,4 +16,4 @@ msg = (
     "Ya puse lo de los mensajes bien, ahora deberia ir sin problemas :) :) :)\n "
     "¿ya ves que guay?\nMuchos chus y ten un dia super, super bueno."
     )
-server.send_email(['satinfivee@gmail.com', ], 'Good morning!', msg)
+server.send_email([to_email, ], 'Good morning!', msg)
